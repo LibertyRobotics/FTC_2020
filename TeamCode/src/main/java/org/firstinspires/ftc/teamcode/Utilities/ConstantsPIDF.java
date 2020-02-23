89814da1b01d8d338cb4bbae74b1096efe2b2441
Avi lance <wardingsoldier@gmail.com>
2020-02-22 23:12:45 -0800

    Made it so that the constants will automatically tune based off the set max velocity.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/ConstantsPIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/ConstantsPIDF.java
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode.Utilities;
 
 public class ConstantsPIDF {
         // Turning PID constants
-        private static final double MAX_VELOCITY = 3420;
-        public static final double TURN_P = 5.2;
-        public static final double TURN_I = 0;
+        //3520 ticks/s is the Max velocity on a full battery
+        private static final double MAX_VELOCITY = 3520;
+        public static final double TURN_P = TURN_F*.1;
+        public static final double TURN_I = TURN_P*.1;
         public static final double TURN_D = 0;
         public static final double TURN_F = 32767/MAX_VELOCITY;
 
